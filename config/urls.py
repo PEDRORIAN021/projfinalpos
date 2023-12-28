5a823cde67a6d5a6da07ebdea6165b339768740a
PEDRORIAN021 <leonardo.pedro@escolar.ifrn.edu.br>
2023-12-28 09:38:13 -0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -16,20 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-
+from rest_framework_nested import routers
 from api import views
 
-router = routers.DefaultRouter()
-router.register(r'user', views.UserViewSet)
-router.register(r'todo', views.TodoViewSet)
-router.register(r'comment', views.CommentViewSet)
-router.register(r'post', views.PostViewSet)
+router = routers.SimpleRouter()
+router.register(r'users', views.UserViewSet)
+users_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
+users_router.register(r'todos', views.TodoViewSet, basename='users-todos')
+router.register(r'todos', views.TodoViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'posts', views.PostViewSet)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path(r'', include(users_router.urls)),
 ]
 
 urlpatterns += router.urls
